48f9e302b1617039f9d247e22c1c821425d9c6d0
tjmcclure0501 <tjmcclure0501@gmail.com>
2024-05-07 14:49:21 -0400

    initial version

---
+++ aws_terraform/main.tf
@@ -0,0 +1,148 @@
+terraform {
+  required_version = ">= 0.12"  # Specify the Terraform version
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  # Specify the provider version
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "cdc"
+  }
+}
+
+resource "aws_subnet" "my_subnet_1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidrs[0]
+  availability_zone = var.availability_zones[0]
+
+  tags = {
+    Name = "cdc"
+  }
+}
+
+resource "aws_subnet" "my_subnet_2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidrs[1]
+  availability_zone = var.availability_zones[1]
+
+  tags = {
+    Name = "cdc"
+  }
+}
+
+resource "aws_security_group" "my_ec2_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  name        = "myec2-security-group"
+  description = "Allow SSH and ICMP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cdc"
+  }
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.ec2_instance_type
+  subnet_id     = aws_subnet.my_subnet_1.id
+  security_groups = [aws_security_group.my_ec2_security_group.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y postgresql
+              EOF
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+resource "aws_db_subnet_group" "mydb_subnet_group" {
+  name       = "mydb-subnet-group"
+  subnet_ids = [aws_subnet.my_subnet_1.id, aws_subnet.my_subnet_2.id]
+
+  tags = {
+    Name = "cdc"
+  }
+}
+
+resource "aws_security_group" "mydb_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  name        = "mydb-security-group"
+  description = "Allow PostgreSQL"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cdc"
+  }
+}
+
+resource "aws_db_instance" "mydb" {
+  allocated_storage    = var.db_allocated_storage
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = var.postgres_version
+  instance_class       = var.db_instance_type
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.postgres13"
+
+  db_subnet_group_name   = aws_db_subnet_group.mydb_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.mydb_security_group.id]
+
+  skip_final_snapshot = true
+
+  tags = {
+    Name = "cdc"
+  }
+}
---
+++ aws_terraform/variables.tf
@@ -0,0 +1,71 @@
+variable "region" {
+  description = "The AWS region to deploy resources."
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidrs" {
+  description = "List of CIDR blocks for the subnets."
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones for the subnets."
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
+}
+
+variable "db_instance_type" {
+  description = "The instance type for the RDS database."
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "ec2_instance_type" {
+  description = "The instance type for the EC2 instance."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "postgres_version" {
+  description = "The version of PostgreSQL for the RDS instance."
+  type        = string
+  default     = "13.3"
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance."
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "db_allocated_storage" {
+  description = "The allocated storage size for the RDS instance in GB."
+  type        = number
+  default     = 20
+}
+
+variable "db_username" {
+  description = "The username for the RDS database."
+  type        = string
+  default     = "myusername"
+}
+
+variable "db_password" {
+  description = "The password for the RDS database."
+  type        = string
+  default     = "mypassword"
+}
+
+variable "db_name" {
+  description = "The name of the RDS database."
+  type        = string
+  default     = "mydatabase"
+}
